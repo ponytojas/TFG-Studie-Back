c9152a56c8f35cef7e029c6c6493224c775539c7
ponytojas <daniel.villalobosdel@gmail.com>
2021-08-15 18:54:28 +0200

    🐛  Debug trying to get wht's happening in prod

--- routes/router.js
+++ routes/router.js
@@ -12,16 +12,20 @@ const pool = new Pool({
   },
 });
 
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
 router.get("/", (req, res, next) => {
   res.send("There are not the droid you're lookin for...");
 });
 
 router.post("/saveData", async (req, res, next) => {
-  try{
-    console.log("Received request");
+  console.log("Received request");
 
-    const client = await pool.connect();
-
+  const client = await pool.connect();
+  try {
     let update = await client
       .query("SELECT * from responses where id = $1", [req.body.id])
       .then((res) => res.rows.length > 0)
@@ -32,11 +36,15 @@ router.post("/saveData", async (req, res, next) => {
       : `INSERT INTO responses (id, ${req.body.question}) VALUES ($1, $2)`;
     let values = [req.body.id, req.body.response];
 
+    console.log({ QUERY: query, VALUES: {values} });
+
     client.query(query, values);
 
     res.sendStatus(200);
-  } catch(err) { }
-  finally{
+    console.log("Response sent");
+  } catch (err) {
+  } finally {
+    console.log("Releasing client");
     client.release();
   }
 });
